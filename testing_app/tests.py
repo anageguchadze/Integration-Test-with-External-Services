603855cb38db65a9d165b72cd04defec203580e3
Ana <anageguchadze22@gmail.com>
2025-05-09 18:28:03 +0400

    Add tests

--- testing_app/tests.py
+++ testing_app/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.core.cache import cache
+from .models import Event
 
-# Create your tests here.
+class EventCacheTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('events-list')
+
+        self.event = Event.objects.create(
+            title="Sample Event",
+            description="Description of event",
+            date="2025-05-09T10:00:00Z"
+        )
+
+    def test_get_event_and_cache(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        cached_data = cache.get('events')
+        self.assertIsNotNone(cached_data)
+
+    def test_event_update_and_cache_refresh(self):
+        self.event.title = "Updated Event"
+        self.event.save()
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        
+        cached_data = cache.get('events')
+        self.assertIsNotNone(cached_data)
+        self.assertEqual(cached_data[0]['title'], 'Updated Event')
