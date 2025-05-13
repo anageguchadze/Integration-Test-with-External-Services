626430a1b6cad57993c94dec020eead4b7e74a26
Ana <anageguchadze22@gmail.com>
2025-05-13 19:10:42 +0400

    Change EventListView

--- testing_app/views.py
+++ testing_app/views.py
@@ -8,16 +8,14 @@ from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 
 
-@method_decorator(cache_page(600), name='dispatch')
 class EventListView(APIView):
-
     def get(self, request):
         cached_events = cache.get('events')
+        if cached_events:
+            return Response(cached_events)
 
-        if not cached_events:
-            events = Event.objects.all()
-            serializer = EventSerializer(events, many=True)
-            cache.set('events', serializer.data, timeout=600)  
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        cache.set('events', serializer.data, timeout=600)
+        return Response(serializer.data)
 
-        return Response(cached_events, status=status.HTTP_200_OK)
